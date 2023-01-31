f0b2a5bca1772db21a5e3c0a9d67602fea22b212
Dennis Iversen <dennis.iversen@gmail.com>
2023-01-31 16:22:28 +0100

    fix build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-minify css/dark.css > dist/dark.min.js
-minify css/light.css > dist/light.min.js
-minify css/dolphin.css > dist/dolphin.min.js
+minify css/dark.css > dist/dark.min.css
+minify css/light.css > dist/light.min.css
+minify css/dolphin.css > dist/dolphin.min.css
